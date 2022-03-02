ed5a8383135d5e637e622db3f2e2d755ccc94f48
Mariusz 'Mario' Zajac <zajac@gmx.us>
2022-03-02 09:50:40 +0100

    Partial viev and temp data

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -37,6 +37,7 @@ public class CategoryController : Controller
         {
             _db.Categories.Add(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");  
         }
         return View(obj);
@@ -75,6 +76,7 @@ public class CategoryController : Controller
         {
             _db.Categories.Update(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
             return RedirectToAction("Index");
         }
         return View(obj);
@@ -112,7 +114,8 @@ public class CategoryController : Controller
         }
         _db.Categories.Remove(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index");
+            TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
 
            
 
