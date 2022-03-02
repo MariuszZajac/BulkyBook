c911c292fa112cc9ebc7979a1d262ffcac940374
Mariusz 'Mario' Zajac <zajac@gmx.us>
2022-03-02 08:59:57 +0100

    Update button

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -42,4 +42,42 @@ public class CategoryController : Controller
         return View(obj);
        
     }
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id==null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var categoryFromDb = _db.Categories.Find(id);
+       // var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+       // var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+       if (categoryFromDb ==null)
+       {
+           return NotFound();
+       }
+
+        return View(categoryFromDb);
+    }
+    //Post
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name", "The DisplayOrder cannot exactly math the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+
+    }
 }
