1bdd40c6a35f6a4bb8a2fed1bc1fe6583f102107
Mariusz 'Mario' Zajac <zajac@gmx.us>
2022-03-02 08:22:20 +0100

    8:21

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -14,7 +14,32 @@ public class CategoryController : Controller
     }
     public IActionResult Index()
     {
-        IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+        IEnumerable<Category> objCategoryList = _db.Categories;
         return View(objCategoryList);
+    }
+    //GET
+    public IActionResult Create()
+    {
+        
+        return View();
+    }
+    //Post
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name==obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name","The DisplayOrder cannot exactly math the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");  
+        }
+        return View(obj);
+       
     }
 }
