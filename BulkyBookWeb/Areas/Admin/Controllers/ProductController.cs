20708a984a6da9ce50db678df4cd1c12116de258
Mariusz 'Mario' Zajac <zajac@gmx.us>
2022-03-08 18:36:04 +0100

    Product Managment complete

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -74,7 +74,14 @@ public class ProductController : Controller
                 string fileName = Guid.NewGuid().ToString();
                 var uploads = Path.Combine(wwwRootPath,@"images\products");
                 var extensions = Path.GetExtension(file.FileName);
-
+                if (obj.Product.ImageUrl != null)
+                {
+                    var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
                 using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extensions), FileMode.Create))
                 {
                     file.CopyTo(fileStreams);
@@ -82,7 +89,16 @@ public class ProductController : Controller
 
                 obj.Product.ImageUrl = @"\images\products\" + fileName + extensions;
             }
-            _unitOfWork.Product.Add(obj.Product);
+
+            if (obj.Product.Id == 0)
+            {
+                _unitOfWork.Product.Add(obj.Product);
+            }
+            else
+            {
+                _unitOfWork.Product.Update(obj.Product);
+            }
+
             _unitOfWork.Save();
             TempData["success"] = "Product created successfully";
             return RedirectToAction("Index");
@@ -91,42 +107,24 @@ public class ProductController : Controller
 
     }
     //GET
-    public IActionResult Delete(int? id)
-    {
-        if (id == null || id == 0)
-        {
-            return NotFound();
-        }
+    //public IActionResult Delete(int? id)
+    //{
+    //    if (id == null || id == 0)
+    //    {
+    //        return NotFound();
+    //    }
 
-        var coverTypFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+    //    var coverTypFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
 
-        if (coverTypFromDbFirst == null)
-        {
-            return NotFound();
-        }
+    //    if (coverTypFromDbFirst == null)
+    //    {
+    //        return NotFound();
+    //    }
 
-        return View(coverTypFromDbFirst);
-    }
+    //    return View(coverTypFromDbFirst);
+    //}
     //Post
-    [HttpPost,ActionName("Delete")] //change action name 
-    [ValidateAntiForgeryToken]
-
-    public IActionResult DeletePost(int? id)
-    {
-        var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-        if (obj==null)
-        {
-            return NotFound();
-        }
-        _unitOfWork.CoverType.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "CoverType deleted successfully";
-        return RedirectToAction("Index");
-
-           
-
-    }
-
+    
     #region API CALLS
 
     [HttpGet]
@@ -135,6 +133,29 @@ public class ProductController : Controller
         var productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
         return Json(new {data = productList});
     }
+    [HttpDelete]
+
+    public IActionResult Delete(int? id)
+    {
+        var obj = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+        if (obj == null)
+        {
+            return Json(new {success = false, message = "Error while deleting"});
+        }
+        var oldImagePath = Path.Combine(_hostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('\\'));
+        if (System.IO.File.Exists(oldImagePath))
+        {
+            System.IO.File.Delete(oldImagePath);
+        }
+        _unitOfWork.Product.Remove(obj);
+        _unitOfWork.Save();
+        return Json(new { success = true, message = "Success" });
+        
+
+
+
+    }
+
 
     #endregion
 }
