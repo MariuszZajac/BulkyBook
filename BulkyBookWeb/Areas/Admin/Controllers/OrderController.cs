9f166cef0a6cbe41b57e6aceadb4e404ca48a486
Mariusz 'Mario' Zajac <zajac@gmx.us>
2022-03-14 18:10:49 +0100

    order procesing

--- BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,32 +1,59 @@
-﻿using BulkyBook.DataAccess.Repository.IRepository;
+﻿using System.Security.Claims;
+using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
     [Area("Admin")]
+    [Authorize]
     public class OrderController : Controller
     {
         private  readonly IUnitOfWork _unitOfWork;
+        [BindProperty] 
+        public OrderVM OrderVM { get; set; }
         public OrderController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
         public IActionResult Index()
         {
-
-
             return View();
         }
+        public IActionResult Details(int orderId)
+        {
+            OrderVM = new OrderVM()
+            {
+                OrderHeader =
+                    _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderId,
+                        includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == orderId, includeProperties: "Product"),
+
+
+            };
+            return View(OrderVM);
+        }
+
         #region API CALLS
 
         [HttpGet]
         public IActionResult GetAll(string status)
         {
             IEnumerable<OrderHeader> orderHeaders;
-            orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
-
+            if (User.IsInRole (SD.Role_Admin )||( User.IsInRole(SD.Role_Employee)))
+            {
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity) User.Identity;
+                var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(u=>u.ApplicationUserId==claim.Value,
+                    includeProperties: "ApplicationUser");
+            }
             switch (status)
             {
                 case "pending":
