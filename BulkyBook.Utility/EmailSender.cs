1ec9d8ef0cda62e9827069c21aa23663ede493ca
Mariusz 'Mario' Zajac <zajac@gmx.us>
2022-03-16 07:13:33 +0100

    Add authorization and email sender

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+
 
 namespace BulkyBook.Utility
 {
@@ -11,6 +14,22 @@ namespace BulkyBook.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("tutaj@dodajemail.com"));//email to configure
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){Text = htmlMessage};
+
+            //send email to client
+            using (var emailClient = new SmtpClient())
+            {
+                // add smtp to  correct email address 
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                //add email and password 
+                emailClient.Authenticate("tutaj@dodajemail.com", "tutajDodajHasło");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
             return Task.CompletedTask;
         }
     }
--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.IO;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
 
 
 namespace BulkyBookWeb.Areas.Admin.Controllers;
 [Area("Admin")]
+[Authorize(Roles = SD.Role_Admin)]
 public class ProductController : Controller
 
 {
